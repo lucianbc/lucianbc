aff9caa10ebb9769ed8d22aad8a1a8087e8268c1
lucianbc <lucianboaca97@gmail.com>
2022-01-09 11:14:51 +0000

    Proper set up tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  plugins: [
-    "gatsby-plugin-postcss",
-    // ...
-  ],
+  plugins: ["gatsby-plugin-postcss"],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <h1 className="text-3xl">Hello Gatsby!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello Gatsby!</h1>;
 }
