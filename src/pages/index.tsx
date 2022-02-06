fa720b15326d54aa571eef2df45f8e3f61655efd
lucianbc <lucianboaca97@gmail.com>
2022-02-06 18:52:42 +0000

    tailwind play

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello Gatsby!</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
+
+const Header = () => {
+  return (
+    <header className="bg-teal-300 flex content-between justify-between">
+      <p>Lucian Boaca</p>
+      <div className="flex">
+        <p>Blog</p>
+        <p>Photography</p>
+        <p>Career</p>
+      </div>
+    </header>
+  );
+};
+
+const Hero = () => {
+  return (
+    <section>
+      <p>Hi, I'm Lucian</p>
+      <p>
+        I am a software engineer, currently working on putting healthcare in the
+        hands of millions by writing React Native at Babylon Health
+      </p>
+      <p>This is my corner of the internet. All views are my own</p>
+    </section>
+  );
+};
